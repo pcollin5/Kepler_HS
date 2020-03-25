26a720b4f6bc858f25e54bc87c9a0425862379c9
Paul Collins <pncollins5@gmail.com>
2020-03-25 16:17:43 -0400

    more script added

--- kepler_script.R
+++ kepler_script.R
@@ -89,6 +89,90 @@
           
           Hawkins_data_profile <- rbind(Hawkins_dp02_18, Hawkins_dp03_18, Hawkins_dp04_18, Hawkins_dp05_18)
           
-          View(Hawkins_data_profile)
+      #### now pull the tracts that have headstarts located in them ####
           
-      ### need a data frame with headstart location names and census tract numbers ###    
+          hawkins_hs_GEOID <- c(47073050501, 47073050301, 47073050700, 47073050400, 47073050800)
+          
+          hawkins_hs_tracts <- Hawkins_data_profile %>%
+            filter(GEOID %in% hawkins_hs_GEOID)
+          
+      # add the school names to the location data frame #
+          
+          cv <- hawkins_hs_tracts %>%
+            filter(GEOID == 47073050501)
+        
+          school <- rep("Carters Valley", length(cv$GEOID))  
+          
+          cv_2 <- cbind(school, cv)
+          
+          fh <- hawkins_hs_tracts %>%
+            filter(GEOID == 47073050301)
+          
+          school <- rep("Fugate Hill", length(fh$GEOID))
+          
+          fh_2 <- cbind(school, fh)  
+          
+          mb <- hawkins_hs_tracts %>%
+            filter(GEOID == 47073050700)
+          
+          school <- rep("McPheeters Bend", length(mb$GEOID))
+          
+          mb_2 <- cbind(school, mb)
+          
+          sg <- hawkins_hs_tracts %>%
+            filter(GEOID == 47073050400)
+          
+          school <- rep("Surgoinsville", length(sg$GEOID))
+          
+          sg_2 <- cbind(school, sg)
+          
+          kp <- hawkins_hs_tracts %>%
+            filter(GEOID == 47073050800)
+          
+          school <- rep("Kepler", length(kp$GEOID))
+          
+          kp_2 <- cbind(school, kp)
+          
+          pretty_tracts <- rbind(cv_2, fh_2, mb_2, sg_2, kp_2)
+          
+          ####pull out the variables we need ####
+          
+          hs_vars <- c("DP02_0045", "DP02_0046", "DP02_0047", "DP02_0053", "DP02_0071", "DP02_0112", "DP02_0151", "DP02_0152", "DP03_0009", "DP03_0062", "DP03_0073", "DP03_0074", "DP03_0119", "DP03_0121",
+                       "DP05_0005", "DP05_0064")
+          
+          hs_per_vars <- c("DP02_0045P", "DP02_0046P", "DP02_0047P", "DP02_0053P", "DP02_0071P", "DP02_0112P", "DP02_0151P", "DP02_0152P", "DP03_0009P", "DP03_0062P", "DP03_0073P", "DP03_0074P", "DP03_0119P", "DP03_0121P",
+                           "DP05_0005P", "DP05_0064P")
+          
+          hs_vars_names <- c("Grandparent living in household and responsible for children under 1", "Grandparent living in household and responsible for children 1-2 years old", "Grandparent living in household and responsible for children 3-4 years olds",
+                             "Children enrolled in preschool or nursery school", "Citizens with a Disability", "Population ESL", "Households with a Computer", "Households with Broadband internet", "Unemployment Rate", "Median Income",
+                             "Total Households with Cash Public Assistance", "Total Households with SNAP", "Percentage of all families below poverty line", "Percentage of all families below poverty line with children under 5",
+                             "Population Age under 5", "Population Racially White")
+         
+          
+          #### quickly look at percent poverty under 5 and children under 5 
+          
+          df_tracts <- pretty_tracts %>%
+            filter(variable %in% hs_vars)
+          
+          df_per_tracts <- pretty_tracts %>%
+            filter(variable %in% hs_per_vars)
+          
+          df_combined <- cbind(hs_vars_names, df_tracts, df_per_tracts)
+          
+          trimmed_df <- df_combined[,c(1,2,4,6,7,14,15)]
+          
+          ###get rid of location for tables 
+          
+          no_location_trimmed_df <- st_set_geometry(trimmed_df, NULL)
+          
+          ###make new names for table 
+          
+          hs_table_names <- c("Measure", "Head Start Location", "Census Tract", "Count", "Margin of Error", "Percent", "Percent Margin of Error")
+          
+          names(no_location_trimmed_df) <- hs_table_names
+          
+          hawkins_hs_datatable <- datatable(no_location_trimmed_df, caption = "Various Metrics for Hawkins County Census Tracts Containing a Head Start")
+          
+          hawkins_hs_datatable
+          
+          ### test for significance 
